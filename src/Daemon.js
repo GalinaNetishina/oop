c520bfe1fc5b58a4e600a6a87f7b9ca2cd3ef26a
Galina Netishina <netishinagalina@yandex.ru>
2024-06-19 12:54:01 +0300

    daemon add

--- src/Daemon.js
+++ src/Daemon.js
@@ -6,4 +6,18 @@ export class Daemon extends Character {
         this.attack = 40;
         this.defence = 10;
     }
+    set stoned(isStoned) {
+        this.#stoned = isStoned
+    }
+    get stoned(){
+        return this.#stoned
+    }
+    getAttack(distance){
+        this.attack = Math.round((1 - 0.1*(distance-1)) *this.attack);
+        if (this.stoned) {
+            this.attack -=  (Math.log2(distance) * 5);
+        }
+        return Math.round(this.attack);
+    }
+    }
 }
--- src/__tests__/buffer.test.js
+++ src/__tests__/buffer.test.js
@@ -2,5 +2,7 @@ import { buf, getBuffer } from '../buffer';
 
 test('buffer convert to string', () => {
     buf.load(getBuffer());
-    expect(buf.toString()).toEqual('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}')
+    const result = buf.toString()
+    expect(typeof(result)).toBe('string')
+    expect(result).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}')
 })
